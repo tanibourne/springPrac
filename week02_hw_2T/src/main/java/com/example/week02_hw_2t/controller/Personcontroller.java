14f966538cfed986bd972441f7d7d9dbd4ade670
tanibourne <tanibourne@gmail.com>
2022-07-24 17:49:53 +0900

    ADD feature DELETE to personHW

--- week02_hw_2T/src/main/java/com/example/week02_hw_2t/controller/Personcontroller.java
+++ week02_hw_2T/src/main/java/com/example/week02_hw_2t/controller/Personcontroller.java
@@ -3,7 +3,9 @@ package com.example.week02_hw_2t.controller;
 import com.example.week02_hw_2t.domain.Person;
 import com.example.week02_hw_2t.domain.PersonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public class Personcontroller {
 
         return personRepository.findAll();
 
+    }
+    @DeleteMapping("/api/persons/{id}")
+    public Long deletePerson(@PathVariable Long id){
+        personRepository.deleteById(id);
+        return id;
     }
 
 }
