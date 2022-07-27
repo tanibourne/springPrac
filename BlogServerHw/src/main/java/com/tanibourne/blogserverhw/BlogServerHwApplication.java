7d6255e6a4abd511107b8f3223801fcd3884d405
tanibourne <tanibourne@gmail.com>
2022-07-27 17:01:13 +0900

    ADD mySQL, connect RDS

--- BlogServerHw/src/main/java/com/tanibourne/blogserverhw/BlogServerHwApplication.java
+++ BlogServerHw/src/main/java/com/tanibourne/blogserverhw/BlogServerHwApplication.java
@@ -15,19 +15,5 @@ public class BlogServerHwApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogServerHwApplication.class, args);
     }
-
-
-    @Bean
-    public CommandLineRunner demo(FeedRepository feedRepository) {
-        return (args) -> {
-            //jpa 사용법
-            //저장할때 쓰는거 CREATE
-//            String name, String address, String job, int age
-            feedRepository.save(new Feed("titleTest", "author", "password", "commentTEST"));
-            //bean
-
-            // 조회할때 쓰는거 READ 전부 READ
-        };
-
 }
-}
+
